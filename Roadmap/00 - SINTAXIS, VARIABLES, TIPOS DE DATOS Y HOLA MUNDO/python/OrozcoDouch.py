889d520b46755611ce66455b1a139b496099635d
OrozcoDouch <151481139+OrozcoDouch@users.noreply.github.com>
2024-01-02 16:09:31 -0400

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/OrozcoDouch.py
@@ -0,0 +1,34 @@
+# htpps://python.org
+
+# Comentario en una línea
+
+"""
+Esto también es
+un comentario
+en varias lineas
+"""
+
+'''
+Esto también es
+un comentario
+en varias lineas
+'''
+#Creando Variables
+my_variable= "Mi variable"
+my_variable= "Nuevo valor de mi variable"
+
+#Tipo de datos primitivos
+#Tipo Cadena de texto
+Cadena= "Este es una cadena de texto"
+#Tipo Entero
+Entero =  5
+#Tipo Flotante
+Flotante= 5.25
+#Tipo Booleano
+Boolenano= True
+#Tipo String
+String= str("mi otra cadena de texto")
+
+#Imprimiendo por terminal
+print("¡Hola, Python!")
+
